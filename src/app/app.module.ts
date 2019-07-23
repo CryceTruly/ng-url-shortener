f18db34b8b4b4bfed5ff7b73ed84198a688ed5e3
crycetruly <crycetruly@gmail.com>
2019-07-23 10:47:10 +0300

    ft(routing):Add routing to core pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'stats', component: StatsComponent, data: { title: "statistics" } },
+  { path: '', component: HomeComponent, data: { title: "welcome" } },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatsComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NavBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
