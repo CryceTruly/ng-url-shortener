2a064f14d4b8787cdbcd412b4576c3599e6b2092
crycetruly <crycetruly@gmail.com>
2019-08-25 00:26:36 +0300

    Add App.json for review apps

--- server/main.ts
+++ server/main.ts
@@ -4,6 +4,7 @@ import { ApplicationModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
 	app.setGlobalPrefix('api');
-	await app.listen(4200);
+	const port=process.env.PORT || 4200
+	await app.listen(port);
 }
 bootstrap();
